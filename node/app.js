bfc7460831c1e9e31b3f85df7c681f53e3804c95
ANSH <anshgupta.mca20@gmail.com>
2021-05-22 15:22:44 +0530

    connecting node to mysql

--- node/app.js
+++ node/app.js
@@ -1,1 +1,11 @@
-console.log("fsdf");
+var faker = require("faker"),
+  mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "connection",
+});
+/*for (i = 0; i <= 100; i++) {
+  console.log(faker.internet.email());
+  console.log(faker.date.past());
+}*/
